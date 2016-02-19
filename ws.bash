3be32710f36060654c1266e3802909ec9c188082
Zachary Elliott <contact@zell.io>
2016-02-19 13:02:43 -0500

    initial commit

---
+++ ws.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+### ws.bash --- websocket lib for bash
+
+## Copyright (c) 2016 Zachary Elliott
+##
+## Authors: Zachary Elliott <contact@zell.io>
+## URL: https://github.com/zellio/ws.bash
+## Version: 0.1.0
+
+### Commentary:
+
+##
+
+### License:
+
+## Permission is hereby granted, free of charge, to any person obtaining a
+## copy of this software and associated documentation files (the “Software”),
+## to deal in the Software without restriction, including without limitation
+## the rights to use, copy, modify, merge, publish, distribute, sublicense,
+## and/or sell copies of the Software, and to permit persons to whom the
+## Software is furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+## DEALINGS IN THE SOFTWARE.
+
+### Code:
+
+### ws.bash ends here
