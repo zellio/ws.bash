6bee4fa75846fd3f24c2c44ac9e0b4483f511213
Zachary Elliott <contact@zell.io>
2016-03-17 01:44:43 -0400

    Begin code import

--- ws.bash
+++ ws.bash
@@ -34,4 +34,115 @@
 
 ### Code:
 
+function is_function
+{
+	declare -Ff "$1" >/dev/null
+}
+
+function wsb__parse_uri
+{
+	local uri="$1"
+
+	local scheme hier_part query fragment
+	local authority path
+	local user_info username password host port
+
+	scheme="${uri%%:*}"
+	hier_part="${uri#*:}"
+
+	if [[ "$hier_part" =~ '#' ]]; then
+		fragment="${hier_part#*#}"
+		hier_part="${hier_part%%#*}"
+	fi
+
+	if [[ "$hier_part" =~ '?' ]]; then
+		query="${hier_part#*\?}"
+		hier_part="${hier_part%%\?*}"
+	fi
+
+	if [[ "$hier_part" =~ ^// ]]; then
+		hier_part="${hier_part#//}"
+		authority="${hier_part%%/*}"
+		path="/${hier_part#*/}"
+	fi
+
+	if [[ "$authority" =~ '@' ]]; then
+		user_info="${authority%%@*}"
+		username="${user_info%%:*}"
+		password="${user_info##*:}"
+		authority="${authority/$user_info@/}"
+	fi
+
+	if [[ "$authority" =~ ':' ]]; then
+		host="${authority%%:*}"
+		port="${authority##*:}"
+	else
+		host="$authority"
+	fi
+
+	wsb_scheme="$scheme"
+	wsb_username="$username"
+	wsb_password="$password"
+	wsb_host="$host"
+	wsb_port="$port"
+	wsb_path="$path"
+	wsb_query="$query"
+	wsb_fragment="$fragment"
+}
+
+function wsb__socket_connect
+{
+	local -r in_sock="${wsb_buffer_dir}/socat.stdin"
+	local -r out_sock="${wsb_buffer_dir}/socket.stdout"
+	local -i socat_pid
+
+	local remote="tcp:"
+	if [ "$wsb_scheme" = 'wss' ]; then
+		remote="openssl:"
+	fi
+	remote+="$wsb_host:$wsb_port"
+
+	mkfifo "$in_sock" "$out_sock"
+
+	socat "$remote" stdio,ignoreeof,nonblock <"$in_sock" >"$out_sock" &
+	socat_pid="$!"
+
+	wsb_socket_in="$in_sock"
+	wsb_socket_out="$out_sock"
+	wsb_socat_pid="$socat_pid"
+}
+
+
+function wsb__build_header
+{
+	local -r socket_key="${wsb_socket_key:-$(head --bytes=16 /dev/urandom | base64)}"
+	local -r socket_version="${wsb_socket_version:-13}"
+
+	echo -e "\
+GET /$wsb_path HTTP/1.1\r
+Host: $wsb_host\r
+Origin: $wsb_origin\r
+Connection: Upgrade\r
+Upgrade: websocket\r
+Sec-WebSocket-Key: $socket_key\r
+Sec-WebSocket-Version: $socket_version\r
+\r"
+}
+
+function wsb__handshake
+{
+	local handshake_response
+
+	echo -e "$(wsb__build_header)" >"$wsb_socket_in"
+
+	while IFS= read -r line;
+		  line="$(tr -d '\r\n' <<<"$line")";
+		  test -n "$line"; do {
+		handshake_response+="$line\n"
+	}; done <"$wsb_socket_out"
+
+	is_function wsb__handshake_callback &&
+		wsb__handshake_callback "$handshake_response"
+}
+
 ### ws.bash ends here
